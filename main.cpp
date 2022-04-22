5646ae661446aede39ad435731155f7cb7159aff
kingstonv1 <kingstongise@gmail.com>
2022-04-22 18:14:52 -0400

    everything is broken

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@
 #include <vector>
 #include "authkey.h"
 
-std::string getAuthKey();
+nlohmann::json data;
+std::string readBuffer; //String that the JSON will be stored in
+
+std::string getAuthKey();  //Forward declare so the compiler knows to search our headers (authkey.h)
 
 static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
 {
@@ -16,21 +19,21 @@ static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* use
 }
 
 std::string authkey = getAuthKey();
-std::string readBuffer;
 
 void request()
 {
     CURL* curl;
     CURLcode res;
 
-    //Create a list of our headers
+    //Create a list of our headers; One for authorization, the other to tell the program to accept a JSON response
     struct curl_slist* headers = NULL;
     headers = curl_slist_append(headers, authkey.c_str());
     headers = curl_slist_append(headers, "accept: application/json");
 
 
     curl = curl_easy_init();
-    if (curl) {
+    if (curl) 
+    {
         //URL to request to
         curl_easy_setopt(curl, CURLOPT_URL, "https://www.thebluealliance.com/api/v3/team/frc4459/event/2022gacar/matches/simple");
 
@@ -38,62 +41,65 @@ void request()
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
 
-        //Set headers; One for authorization, the other to tell the program to accept a JSON response
+        //Set headers to the list we made earlier
         curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
-        //Set verbose to 1L for extra information
-            //Switched off to not display authkey.
-        curl_easy_setopt(curl, CURLOPT_VERBOSE, NULL);
-
-
         res = curl_easy_perform(curl);
-        /* Check for errors */
+        
+        // Check for errors
         if (res != CURLE_OK)
             fprintf(stderr, "curl_easy_perform() failed: %s\n",
                 curl_easy_strerror(res));
 
 
-        curl_easy_cleanup(curl);
-
-    }
-
+        curl_easy_cleanup(curl); 
+    } 
 }
-
-class jstuff 
-{
-    public:
-    using json = nlohmann::json;
-    json data = json::parse(readBuffer);
-};
-jstuff jbj;
 
 class info 
 {
     private:
 
-    int amountMatches {0};
+    int amountMatches = data.size();
     std::vector <std::string> redList;
     std::vector <std::string> blueList;
     std::vector <int> matchNumbers;
     
     public:
-        
-    void countMatches() { for ( auto x : jbj.data.items() ) { amountMatches++; } }
 
     void getData() 
     {
-    for ( int i {0}; i < amountMatches; i++ ) 
-        {
-            matchNumbers.push_back(jbj.data[i]["match_number"]);
+        for ( int i {0}; i < amountMatches; i++ ) 
+            {
+                matchNumbers.push_back(data[i]["match_number"]);
 
-            for (int x{0}; x < jbj.data[i]["alliances"]["blue"]["team_keys"].size(); x++) 
-                { blueList.push_back(jbj.data[i]["alliances"]["blue"]["team_keys"][x]); }
-            
-            for (int x{0}; x < jbj.data[i]["alliances"]["red"]["team_keys"].size(); x++) 
-                { redList.push_back(jbj.data[i]["alliances"]["red"]["team_keys"][x]); }
-        } 
+                for (int x{0}; x < data[i]["alliances"]["blue"]["team_keys"].size(); x++) 
+                    { blueList.push_back(data[i]["alliances"]["blue"]["team_keys"][x]); }
+                
+                for (int x{0}; x < data[i]["alliances"]["red"]["team_keys"].size(); x++) 
+                    { redList.push_back(data[i]["alliances"]["red"]["team_keys"][x]); }
+            } 
     }
 
+    void printMatches() { std::cout << amountMatches << "\n"; }
+
+    void printData() 
+    {
+        for (int i{0}; i < amountMatches; i++ ) 
+        {
+            std::cout << "Match number " << matchNumbers[i] << ":\n";
+            
+            std::cout << "Blue: ";
+            for (int x = (i * 3 - 2); x < (i * 3 + 1); i++ )
+            { std::cout << blueList[i] << ", "; }
+            std::cout << "\n" << "\n";
+
+            std::cout << "Red: ";
+            for (int x = (i * 3 - 2); x < (i * 3 + 1); i++ )
+            { std::cout << redList[i] << ", "; }
+            std::cout << "\n" << "\n";
+        }
+    }
 };
 
 info inf;
@@ -101,28 +107,25 @@ info inf;
 int main()
 {
     using json = nlohmann::json;
-
-    
-    request();
-    inf.countMatches();
-    inf.getData();
     
     try
     {
-        //This prints the raw response unparsed. Useful because it is formatted whereas the JSON is not.
-        //std::cout << readBuffer << std::endl;
+    //This prints the raw response unparsed. Useful because it is formatted whereas the JSON is not.
+    std::cout << readBuffer << std::endl;
 
+    request();
     
+    json data = json::parse(readBuffer);
     
-    }
-    
+    inf.getData();
+    inf.printData();
+    inf.printMatches();
+    } 
+       
     catch (json::exception& e)
     {
         std::cout << e.what() << '\n';
     }
-
-    
-
 
     return 0;
 }
