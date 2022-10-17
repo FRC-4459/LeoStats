39f2d961c8871b89fbcb9822fa2437a54f25c1c1
kingstonv1 <kingstongise@gmail.com>
2022-10-16 21:39:51 -0400

    Start RequireJS implementation

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,18 @@
-import Axios from 'axios';
-import game from './game.js';
+require.config({
+    "packages": ["axios", "game"],
+    "paths": 
+    {
+        axios: "/home/kingstonv/projects/LeoWeb/node_modules/axios/index",
+        game: "/home/kingstonv/projects/LeoWeb/scripts/game"
+    }
+});
+
+define(function (axios, game) {
+    var Axios = require('axios');
+    var game = require('./game');
+});
+
+var Axios = require(['axios'], function ());
+
 
 Axios.get('https://api.sampleapis.com/switch/games').then((res) => console.log(res)).catch((err) => console.err(err));
