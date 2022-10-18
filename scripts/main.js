e956013558d5be43146c7589b6405a09921a3b3d
kingstonv1 <kingstongise@gmail.com>
2022-10-18 19:47:21 -0400

    working imports + no more node

--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,1 @@
-require.config({
-    "packages": ["axios", "game"],
-    "paths": 
-    {
-        axios: "/home/kingstonv/projects/LeoWeb/node_modules/axios/index",
-        game: "/home/kingstonv/projects/LeoWeb/scripts/game"
-    }
-});
 
-define(function (axios, game) {
-    var Axios = require('axios');
-    var game = require('./game');
-});
-
-var Axios = require(['axios'], function ());
-
-
-Axios.get('https://api.sampleapis.com/switch/games').then((res) => console.log(res)).catch((err) => console.err(err));
