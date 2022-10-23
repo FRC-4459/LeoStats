9fa727c4d0b99f698e62a356211a250f2eb629c8
kingstonv1 <kingstongise@gmail.com>
2022-10-23 18:21:58 -0400

    add check for which team the user is in

--- scripts/game.js
+++ scripts/game.js
@@ -2,10 +2,22 @@ class game
 {
     matchNumber = 0;
     matchTime = 0;
-    userParticipating = false;
+    userTeam = "none";
     blue = [0, 0, 0];
     red = [0, 0, 0];
     matchType = 'N/A';
+
+    isParticipating(teamNum) {
+        for (let i = 0; i < 2; i++) { 
+            if (blue[i].search(teamNum) != -1) 
+                { return "blue"; }
+            if (red[i].search(teamNum) != -1) 
+                { return "red"; }
+        }
+
+        return "none";
+    }
+
 }
 
 export default game;
