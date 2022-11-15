fbdfa94cb78a4772f8d48a6ed870983ca13ea3b5
kingstonv1 <kingstongise@gmail.com>
2022-11-15 16:26:35 -0500

    start div customization on render

--- scripts/main.js
+++ scripts/main.js
@@ -3,26 +3,13 @@ import game from './game.js';
 let gameTemp = document.getElementById("gameTemp");
 let infoArea = document.getElementById("infoArea");
 
-function renderGameDiv(game) {
-    let newGame = gameTemp.content.cloneNode(true);
-    let gameDiv = newGame.childNodes.item(1);
-    let blueTeam = gameDiv.childNodes.item(1);
-    
-    console.log(blueTeam.childNodes);
-
-    for (let i = 0; i < 3; i++) {
-        blueTeam.childNodes.item((4 * i) + 1).innerText = game.blue[i];
-    }
-
-    infoArea.appendChild(newGame);
-}
 
 async function createGames() {
     let teamNum = document.getElementById("teamNum").value;
     let eventCode = document.getElementById("eventCode").value;
     let res = await axios.get(`http://localhost:8000/events/byTeam?teamNum=${teamNum}&eventCode=${eventCode}`);
     let games = [];
-
+    
     for (let i = 0; i < res.data.length; i++) { 
         let currentGame = new game;
         currentGame.matchTime = res.data[i].autoStartTime;
@@ -31,21 +18,39 @@ async function createGames() {
         for (let l = 0; l < res.data[i].teams.length; l++) {
             if (l < 3) { 
                 currentGame.red[l] = res.data[i].teams[l].teamNumber }
-            else { 
-                currentGame.blue[l - 3]  = res.data[i].teams[l].teamNumber }
-        }
+                else { 
+                    currentGame.blue[l - 3]  = res.data[i].teams[l].teamNumber }
+                }
+                
+                currentGame.userTeam = currentGame.isParticipating(teamNum);
+                games.push(currentGame);
+            }
+            
+            console.log(`Got ${games.length} games from ${eventCode}.`)
+            
+            for (let i = 0; i < games.length; i++) {
+                renderGameDiv(games[i]);
+            }
+            console.log("Rendered Divs.");
+            return games;
+        };
+
+function renderGameDiv(game) {
+    let newGame = gameTemp.content.cloneNode(true);
+    let gameDiv = newGame.childNodes.item(1);
+    let blueTeam = gameDiv.childNodes.item(1);
+    //let redTeam = gameDiv.childNodes.item(5);
+
+    if (game.isParticipating === "blue") {
         
-        currentGame.userTeam = currentGame.isParticipating(teamNum);
-        games.push(currentGame);
     }
     
-    console.log(`Got ${games.length} games from ${eventCode}.`)
-
-    for (let i = 0; i < games.length; i++) {
-        renderGameDiv(games[i]);
+    for (let i = 0; i < 3; i++) {
+        blueTeam.childNodes.item((4 * i) + 1).innerText = game.blue[i];
+        //redTeam.childNodes.item((4 * i) + 1).innerText = game.red[i];
     }
-    console.log("Rendered Divs.");
-    return games;
-};
 
-document.getElementById("submitButton").addEventListener("click", createGames);
+    infoArea.appendChild(newGame);
+}
+
+        document.getElementById("submitButton").addEventListener("click", createGames);
