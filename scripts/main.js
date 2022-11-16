79b296c0dfff30677720c9419b7dbed9bbfd9b33
kingstonv1 <kingstongise@gmail.com>
2022-11-16 12:47:38 -0500

    start with sorting games before display

--- scripts/main.js
+++ scripts/main.js
@@ -1,65 +1,73 @@
 import game from './game.js';
 
-let gameTemp = document.getElementById("gameTemp");
-let infoArea = document.getElementById("infoArea");
+let gameTemp = $(gameTempl);
+let infoArea = $(infoAreaDiv);
 
+function sortGames(games) {
+
+
+
+
+
+    return games;
+}
 
 async function createGames() {
-    let teamNum = document.getElementById("teamNum").value;
-    let eventCode = document.getElementById("eventCode").value;
-    let res = await axios.get(`http://localhost:8000/events/byTeam?teamNum=${teamNum}&eventCode=${eventCode}`);
+    let frcNum = $(teamNum).val();
+    let frcCode = $(eventCode).val();
+    let res = await axios.get(`http://localhost:8000/events?teamNum=${frcNum}&eventCode=${frcCode}`);
     let games = [];
     
     for (let i = 0; i < res.data.length; i++) { 
         let currentGame = new game;
         currentGame.matchTime = res.data[i].autoStartTime;
         currentGame.matchNumber = res.data[i].matchNumber;
+        currentGame.description = res.data[i].description;
         
         for (let l = 0; l < res.data[i].teams.length; l++) {
-            if (l < 3) { 
-                currentGame.red[l] = res.data[i].teams[l].teamNumber }
-                else { 
-                    currentGame.blue[l - 3]  = res.data[i].teams[l].teamNumber }
-                }
-                
-                currentGame.userTeam = currentGame.isParticipating(teamNum);
-                games.push(currentGame);
-            }
+        
+            if (l < 3) 
+                { currentGame.red[l] = res.data[i].teams[l].teamNumber }
             
-            console.log(`Got ${games.length} games from ${eventCode}.`)
+            else 
+                { currentGame.blue[l - 3]  = res.data[i].teams[l].teamNumber }
+        }
+        
+        currentGame.userTeam = currentGame.isParticipating(frcNum);
+        games.push(currentGame);
+    }
             
-            for (let i = 0; i < games.length; i++) {
-                renderGameDiv(games[i]);
-            }
-            console.log("Rendered Divs.");
-            return games;
-        };
+    console.log(`Got ${games.length} games from ${frcCode}.`)
+    
+    games = sortGames(games);
+
+    console.log(`Sorthed ${games.length} games.`)
+
+    for (let i = 0; i < games.length; i++) 
+        { renderGameDiv(games[i]); console.log(games[i].description); }
+
+    console.log("Rendered Divs.");
+    return games;
+};
 
 function renderGameDiv(game) {
     let newGame = $(gameTemp).clone();
-    let gameDiv = $(newGame.contents()[1]);
-    let blueTeam = $(gameDiv.contents()[1]);
-    let redTeam = $(gameDiv.contents()[3]);
+    let gameContainer = $(newGame.contents()[1]);
+    let blueTeam = $(gameContainer.contents()[3]);
+    let redTeam = $(gameContainer.contents()[5]);
 
-    switch (game.userTeam) {
-        case "blue":
-            gameDiv.css("background-color", "aquamarine");
-            break;
-        case "red":
-            gameDiv.css("background-color", "pink");
-            break;
-        default:
-            gameDiv.css("background-color", "grey");
-            break;
-    };
     
     for (let i = 0; i < 3; i++) {
-        blueTeam.children()[i * 2].innerHTML = game.blue[i];
-        redTeam.children()[i * 2].innerHTML = game.red[i];
+        //The h1 elements we want occur at 1, 5, and 9 in the parent element,
+        //So we must multiply the index by 4, then add 1.
+        $(blueTeam.contents()[(i * 4) + 1]).text(game.blue[i]);
+        $(redTeam.contents()[(i * 4) + 1]).text(game.red[i]);
     }
 
-    infoArea.appendChild(gameDiv.get(0));
+    infoArea.append(gameContainer.get(0));
 }
 
 $(submitButton).on("click", createGames);
-
+let newGame = $(gameTemp).clone();
+let gameDiv = $(newGame.contents()[1])
+infoArea.append(gameDiv.get(0));
