18f29b57248206481aff9a872ed7f0af7b96edd7
kingstonv1 <kingstongise@gmail.com>
2022-11-16 12:37:38 -0500

    matchtype & number -->description

--- scripts/game.js
+++ scripts/game.js
@@ -1,11 +1,10 @@
 class game 
 {
-    matchNumber = 0;
     matchTime = 0;
     userTeam = "none";
     blue = [0, 0, 0];
     red = [0, 0, 0];
-    matchType = 'N/A';
+    description = 'N/A';
 
     isParticipating(teamNum) {
         for (let i = 0; i < 2; i++) { 
