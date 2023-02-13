488ac1303bd94ee32bce480822f854cb88a6639f
kingstonv1 <kingstongise@gmail.com>
2023-02-13 11:31:05 -0500

    Working gamesByEvent Path

--- server/LeoSrv/routes.py
+++ server/LeoSrv/routes.py
@@ -1,9 +1,28 @@
+import os
+import requests as HTTPRequests
+from dotenv import load_dotenv
 from LeoSrv import LeoStats
+from flask import request, Response
+
+load_dotenv()
+
+headers = {
+    'Authorization': f'Basic {os.getenv("FRC_KEY")}',
+    'If-Modified-Since': ''
+        }
 
 @LeoStats.route('/')
 def index():
     return "Hello, World!"
 
-@LeoStats.route('/alternate')
-def alternate():
-    return "This is an alternate page :)"
+@LeoStats.route('/gamesByEvent')
+def gamesByEvent():
+    eventCode = request.args.get('event')
+    
+    if eventCode == None:
+        return Response('You must include an event code in your querystring.', 400)
+    
+    res = HTTPRequests.get(f'https://frc-api.firstinspires.org/v3.0/2022/matches/{eventCode}', headers=headers)
+    print(f'FRC Request returned {res.status_code} - {res.reason}')
+
+    return Response(res.text, 200)
